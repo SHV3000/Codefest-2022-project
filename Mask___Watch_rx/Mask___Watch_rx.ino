e0e521fe73de3f5133a69ed514bac7bab2dd6fa0
SHV3000 <98201462+SHV3000@users.noreply.github.com>
2022-01-22 14:19:40 +0530

    LCD code done

--- Mask___Watch_rx/Mask___Watch_rx.ino
+++ Mask___Watch_rx/Mask___Watch_rx.ino
@@ -1,7 +1,19 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <Servo.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 32 
+#define OLED_RESET     4 
+#define SCREEN_ADDRESS 0x3C 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
 int value = 0;
 RF24 radio(7, 8); 
 const byte addresses[][6] = {"00001"};
@@ -23,10 +35,18 @@ void loop() {
 
       if (value == 1) {
        Serial.println("Safe");
+  display.setTextSize(3);           
+  display.setTextColor(SSD1306_WHITE);      
+  display.setCursor(0,0);           
+  display.println("SAFE");
         }
 
         else if (value == 2){
           Serial.println("UnSafe");
+  display.setTextSize(2);           
+  display.setTextColor(SSD1306_WHITE);      
+  display.setCursor(0,0);           
+  display.println("UNSAFE");
           }
         }
 }
--- Mask___Watch_tx/Mask___Watch_tx.ino
+++ Mask___Watch_tx/Mask___Watch_tx.ino
@@ -3,10 +3,25 @@
 #include <RF24.h>
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+#define OLED_RESET     4 
+#define SCREEN_ADDRESS 0x3D 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define NUMFLAKES     10 
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
 #define REPORTING_PERIOD_MS     1000
 PulseOximeter pox;
 uint32_t tsLastReport = 0;
 int value = 0;
+int button = 7;
+int button1 = 8;
+
 void onBeatDetected()
 {
    Serial.println("Beat!");
@@ -17,6 +32,8 @@ RF24 radio(7, 8);
 const byte addresses[][6] = {"00001"};
 
 void setup() {
+  pinMode(button, INPUT);
+  pinMode(button1, INPUT);
   radio.begin();
   radio.openWritingPipe(addresses[0]); 
   radio.setPALevel(RF24_PA_MIN);
@@ -42,10 +59,27 @@ void loop() {
         Serial.print(pox.getSpO2());
         tsLastReport = millis();    
         }
-if (pox.getSpo2() > 95){
+
+if (button == 1) {
+  
+  }
+
+else if (button1 == 1) {
+    display.clearDisplay();
+  display.setTextSize(1);           
+  display.setTextColor(SSD1306_WHITE);      
+  display.setCursor(0,0);           
+  display.println("SpO2:");
+  display.setCursor(0,10);           
+  display.println(pox.getSpO2());
+  display.setCursor(30,10);           
+  display.println("%");  
+  }
+        
+if (pox.getSpO2() > 95){
   value = 1;
   }
-else if (pox.getSpo2() < 95) {
+else if (pox.getSpO2() < 95) {
   value = 2;
   }
 
